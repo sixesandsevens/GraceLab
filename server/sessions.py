f62ce3ed52ba95231ee20b7a1400aafade6e8130
sixesandsevens <darkseren@gmail.com>
2026-06-05 16:19:34 -0400

    ?

--- server/sessions.py
+++ server/sessions.py
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, NumberRange, Optional
 from werkzeug.security import generate_password_hash
+from weasyprint import HTML as WeasyprintHTML
 from extensions import db
 from models import Session, SessionEvent, Setting
 
@@ -212,8 +213,7 @@ def batch():
         html = render_template("batch_print.html", sessions=sessions,
                                org_name=org_name, ticket_footer=ticket_footer)
 
-        from weasyprint import HTML
-        pdf = HTML(string=html, base_url=request.host_url).write_pdf()
+        pdf = WeasyprintHTML(string=html, base_url=request.host_url).write_pdf()
 
         return Response(
             pdf,
